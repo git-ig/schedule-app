f16ebbf775734263b90cb7fb2190ca65f3fa687a
git-ig <ivan.pavlovych@gmail.com>
2025-07-02 19:14:05 +0300

    update github actions

--- infra/terraform/gcp/variables.tf
+++ infra/terraform/gcp/variables.tf
@@ -28,3 +28,8 @@ variable "ssh_public_key_path" {
   description = "The path to the public SSH key."
   type        = string
 }
+
+variable "bucket_name" {
+  description = "The name of the GCS bucket for Terraform state."
+  type        = string
+}
