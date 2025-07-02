ed2859bc6976e3c72ae1e5713ce9d5c3cc613bcd
git-ig <ivan.pavlovych@gmail.com>
2025-07-03 01:19:06 +0700

    update github actions

--- infra/terraform/gcp/outputs.tf
+++ infra/terraform/gcp/outputs.tf
@@ -25,13 +25,35 @@ output "monitoring_private_ip" {
 
 output "ansible_inventory" {
   description = "Ansible inventory file content."
-  value = templatefile("${path.module}/inventory.tpl", {
-    bastion_public_ip     = module.compute.bastion_public_ip
-    frontend_private_ip   = module.compute.frontend_private_ip
-    backend_private_ip    = module.compute.backend_private_ip
-    database_private_ip   = module.compute.database_private_ip
-    monitoring_private_ip = module.compute.monitoring_private_ip
-    ssh_user              = var.ssh_user
-  })
+  value = <<-EOT
+    [bastion]
+    bastion ansible_host=${module.compute.bastion_public_ip}
+
+    [frontend]
+    frontend ansible_host=${module.compute.frontend_private_ip}
+
+    [backend]
+    backend ansible_host=${module.compute.backend_private_ip}
+
+    [database]
+    database ansible_host=${module.compute.database_private_ip}
+
+    [monitoring]
+    monitoring ansible_host=${module.compute.monitoring_private_ip}
+
+    [all:vars]
+    ansible_user=${var.ssh_user}
+    ansible_ssh_private_key_file=~/.ssh/id_rsa
+    ansible_ssh_common_args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+    [private_instances:children]
+    frontend
+    backend
+    database
+    monitoring
+
+    [private_instances:vars]
+    ansible_ssh_common_args="-o ProxyCommand='ssh -W %h:%p -q ${var.ssh_user}@${module.compute.bastion_public_ip}' -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+  EOT
   sensitive = true
 }
