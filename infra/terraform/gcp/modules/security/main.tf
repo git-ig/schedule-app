7e8e181b24773f1289c4888aca7f3de06f51a0c1
git-ig <ivan.pavlovych@gmail.com>
2025-07-06 18:55:02 +0300

    update monitoring config

--- infra/terraform/gcp/modules/security/main.tf
+++ infra/terraform/gcp/modules/security/main.tf
@@ -33,3 +33,18 @@ resource "google_compute_firewall" "allow_http_to_bastion" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["bastion"]
 }
+
+resource "google_compute_firewall" "allow_monitoring" {
+  name    = "allow-monitoring"
+  network = var.network_name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9090", "3000", "9100", "8081"]
+  }
+
+  source_ranges = [
+    var.public_subnet_cidr,
+    var.private_subnet_cidr
+  ]
+}
