e9ca51738035ba27bd65cf82487085073ce80c7b
git-ig <ivan.pavlovych@gmail.com>
2025-07-06 12:25:09 +0300

    update frontend config

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
-echo "Replacing REACT_APP_API_BASE_URL_PLACEHOLDER with http://10.0.2.33:8080/"
+URL="${REACT_APP_API_BASE_URL:-http://10.0.2.33:8080/}"
+case "$URL" in
+  */) BACKEND_URL="$URL" ;;
+  *)   BACKEND_URL="${URL}/" ;;
+esac
 
-# change placeholder to real URL backend
-find /usr/share/nginx/html -name "main.*.js" -exec sed -i "s|REACT_APP_API_BASE_URL_PLACEHOLDER|http://10.0.2.33:8080/|g" {} \;
+echo "Replacing REACT_APP_API_BASE_URL_PLACEHOLDER with ${BACKEND_URL}"
+
+find /usr/share/nginx/html -type f \( -name "*.js" -o -name "*.html" \) \
+  -exec sed -i "s|REACT_APP_API_BASE_URL_PLACEHOLDER|${BACKEND_URL}|g" {} +
 
 echo "Replacement completed"
 
-# run nginx
 exec nginx -g "daemon off;"
