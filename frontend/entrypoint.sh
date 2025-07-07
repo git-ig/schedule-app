5467904be364d4a748867876c63b93bea1d839cc
git-ig <ivan.pavlovych@gmail.com>
2025-07-07 11:43:34 +0300

    update database config

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 set -e
 
-URL="${REACT_APP_API_BASE_URL:-https://dock.ink/}"
-case "$URL" in
-  */) BACKEND_URL="$URL" ;;
-  *)   BACKEND_URL="${URL}/" ;;
-esac
+find /usr/share/nginx/html -name "*.js" -exec sed -i 's|__API_BASE_URL__|https://api.dock.ink/|g' {} \;
 
-find /usr/share/nginx/html -type f \( -name "*.js" -o -name "*.html" \) \
-  -exec sed -i "s|__API_BASE_URL__|${BACKEND_URL}|g" {} +
-
-echo "Files processed:"
-find /usr/share/nginx/html -type f \( -name "*.js" -o -name "*.html" \) -exec echo "  {}" \;
-
+echo "Replacement completed. Starting nginx..."
 exec nginx -g "daemon off;"
