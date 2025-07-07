766f6210401deed93a651e0247740f17c4fc671f
git-ig <ivan.pavlovych@gmail.com>
2025-07-07 12:47:21 +0300

    update database config

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-find /usr/share/nginx/html/static/js/ -name "main.*.js" -exec sed -i 's|http://localhost:8080/|https://api.dock.ink/|g' {} \;
+find /usr/share/nginx/html/static/js/ -name "main.*.js" -exec sed -i 's|REACT_APP_API_BASE_URL|https://api.dock.ink/|g' {} \;
 
 echo "Replacement completed. Starting nginx..."
 exec nginx -g "daemon off;"
